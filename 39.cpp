baa8f70f288fca387dfeeb13daa89eb8aa53b0ea
Haozl <1219735011@qq.com>
2019-09-19 19:44:31 +0800

    平衡二叉树

--- 39.cpp
+++ 39.cpp
@@ -18,20 +18,20 @@ struct TreeNode{
 class Solution {
 public:
     bool isBalenced(TreeNode* root){
-        int high = 0;
+        int high;
         return isBalenced(root, high);
     }
-    bool isBalenced(TreeNode* root, int &high){
-        if(!root){
-            high == 0;
-            return true;
+    bool isBalenced(TreeNode* root, int &high){//重载，判断函数， 加上用以记录节点高度的参数
+        if(!root){//当是叶子节点时
+            high == 0;//高度为0
+            return true;//是平衡二叉树
         } 
-        int left, right;
-        if(isBalenced(root->left, left) && isBalenced(root->right, right)){
+        int left, right;//用来记录左右子树的高度
+        if(isBalenced(root->left, left) && isBalenced(root->right, right)){//后序遍历
             int sub = abs(left - right);
-            if(sub < 2){
-                high = 1 + max(left, right);
-                return true;
+            if(sub < 2){//如果左右子树高度差小于2
+                high = 1 + max(left, right);//更新本层高度
+                return true;//是平衡二叉树
             }
         }
         return false;
