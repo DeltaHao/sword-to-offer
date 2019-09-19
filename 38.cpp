677593802749a10bd777e9fdaccd91714b12ca94
Haozl <1219735011@qq.com>
2019-09-19 18:24:09 +0800

    二叉树的深度

---
+++ 38.cpp
@@ -0,0 +1,23 @@
+/*二叉树的深度
+*/
+#include<iostream>
+
+using namespace std;
+
+#define max(a, b) a>b?a:b
+struct TreeNode{
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x):
+        val(x), left(NULL), right(NULL){}
+};
+
+class Solution {
+public:
+    int TreeDepth(TreeNode* root){//基本操作
+        if(!root) return 0;
+        if(!root->left && !root->right) return 1;
+        return 1 + max(TreeDepth(root->left), TreeDepth(root->right));
+    }
+};
